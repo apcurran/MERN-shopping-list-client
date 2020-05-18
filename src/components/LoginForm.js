5291b1109de31f4f785805769157760ccd5d39e1
apc <alexcurran.va@gmail.com>
2020-05-18 08:50:13 -0500

    Fixed login bug

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,41 +1,69 @@
 import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
 import { useHistory } from "react-router-dom";
 
 export default function LoginForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [triggerCall, setTriggerCall] = useState(false);
+    const [error, setError] = useState("");
 
     let history = useHistory();
 
-    async function handleSubmit(event) {
+    useEffect(() => {
+        async function fetchData() {
+            if (!triggerCall) return; // Only run hook on triggering form submit
+
+            const API_URL = "/api/user/login";
+
+            const options = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                  email,
+                  password
+                })
+            };
+    
+            try {
+                
+                const response = await fetch(API_URL, options);
+                const data = await response.json();
+                
+                // Return from function if an error has been set
+                if (data.hasOwnProperty("message")) {
+                    setError(data.message);
+                    setTriggerCall(false);
+
+                    return;
+                }
+
+                const token = data;
+    
+                localStorage.setItem("authToken", token);
+                setTriggerCall(false); // Reset trigger
+                history.push("/user/list");
+    
+            } catch (err) {
+                setTriggerCall(false); // Reset trigger
+                console.error(err);
+            }
+        }
+
+        fetchData();
+    }, [triggerCall]);
+
+
+    function handleSubmit(event) {
         event.preventDefault();
 
-        const data = JSON.stringify({
-            email,
-            password
-        });
-
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
-
-        try {
-            const loggedInUser = await Axios.post("/api/user/login", data, config);
-            const token = loggedInUser.data;
-
-            localStorage.setItem("authToken", token);
-            // Redirect to shopping list view
-            history.push("/user/list");
-        } catch (err) {
-            console.error(err);
-        }
+        setTriggerCall(true);
     }
 
     return (
         <form onSubmit={handleSubmit} className="login-form form">
+          {error ? (
+              <h3 className="form__error">{error}</h3>
+          ) : null }
           <div className="form-group">
               <label htmlFor="login-name" className="form-group__label">Email</label>
               <input
